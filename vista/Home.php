68783b97c185d0cdc8936cc997df50c1228ea91d
JenniferSysCoder <jenniferguerra348@gmail.com>
2024-04-20 09:19:49 -0600

    Arreglando css
    las etiquetas ya funcionan

--- vista/Home.php
+++ vista/Home.php
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
 </head>
-<body>
+<body class="cuerpo">
     <header class="titulo">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid">
@@ -31,7 +31,7 @@
                             <a class="nav-link active" href="#">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#" >Reglas</a>
+                            <a class="nav-link" onclick='mostrarReglas()' href="#" >Reglas</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Acerca de</a>
