1dc4b483331c513f37999f0e64f0bffd0d4e9cbd
Gilberto Menendez <39131041+GilbertoMenendez11@users.noreply.github.com>
2024-05-01 11:52:05 -0600

    modificacion en el archivo de conexion
    conexion arreglada para gilberto

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion=new mysqli("localhost","root","daniel","casinoruleta","3306");
+    $conexion=new mysqli("localhost","root","12345","casinoruleta","3306");
     $conexion->set_charset("utf8");
 
     /* if($conexion->connect_error){
