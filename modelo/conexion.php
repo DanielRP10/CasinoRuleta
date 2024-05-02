7fafe2c330984286d0716c99b16856be44e62406
Gilberto Menendez <gilbertomenendez5@gmail.com>
2024-05-01 18:33:23 -0600

    111
    111

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,10 +1,10 @@
 <?php
-    $conexion=new mysqli("localhost","root","2004abril16jgf","casinoruleta","3306");
+    $conexion=new mysqli("localhost","root","12345","casinoruleta","3306");
     $conexion->set_charset("utf8");
 
-    /* if($conexion->connect_error){
+     if($conexion->connect_error){
         die("Conexion fallida". $conexion->connect_error);
     }else{
         echo 'Conectado';
-    } */
+    } 
 ?>
