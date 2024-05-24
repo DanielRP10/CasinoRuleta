4f67ebd9897557736e860feeacf012b93af09da6
Gilberto Menendez <gilbertomenendez5@gmail.com>
2024-05-23 18:22:25 -0600

    hh

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< Updated upstream
     $conexion=new mysqli("localhost","root","daniel","casinoruleta","3306");
+=======
+    $conexion=new mysqli("localhost","root","12345","casinoruleta","3306");
+>>>>>>> Stashed changes
     $conexion->set_charset("utf8");
 
     /*  if($conexion->connect_error){
