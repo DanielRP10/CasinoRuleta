10f37fe82c9d8398ff48d908d15c3e3d69b6f178
JenniferSysCoder <jenniferguerra348@gmail.com>
2024-05-27 23:56:44 -0600

    Cambio de reglas y algunos otros estilos
    Cambio de reglas

--- js/javascrip.js
+++ js/javascrip.js
@@ -1,24 +1,28 @@
 function mostrarReglas() {
-    Swal.fire({/*Swal.fire: se utiliza para mostrar una pantalla emergente*/
+    Swal.fire({
         title: 'REGLAS DEL JUEGO', 
-        html: '<ol>' +
-        '<li>Gana puntos al apostar en las figuras correctas y acertar en la ruleta.</li><br>' +
-        '<li>Tienes un tablero con 16 figuras para apostar.</li><br>' +
+        html: '<ol style="color: white;">' + // Color del texto establecido a negro
+        '<li>Gana puntos al apostar en los números correctos y acertar en la ruleta.</li><br>' +
+        '<li>Tienes una ruleta con 16 números para apostar</li><br>' +
         '<li>Dispones de 5 intentos para apostar.</li><br>' +
-        '<li>En cada intento, coloca una ficha en una figura del tablero.</li><br>' +
-        '<li>La ruleta gira después de realizar las apuestas.</li><br>' +
-        '<li>Si la ruleta se detiene en una figura en la que apostaste, ganas puntos; de lo contrario, no ganas puntos.</li><br>' +
-        '<li>La cantidad de puntos ganados depende de la figura y el tablero.</li><br>' +
+        '<li>En cada intento, elige un número en la ruleta.</li><br>' +
+        '<li>La ruleta gira después de realizar la selección del número.</li><br>' +
+        '<li>Si la ruleta se detiene en el número que apostaste, ganas 50 puntos; de lo contrario, no ganas puntos.</li><br>' +
+        '<li>La cantidad de puntos es de 50 por cada número.</li><br>' +
         '<li>El juego termina después de 5 intentos.</li><br>' +
         '<li>Se muestra tu puntuación total después de cada juego.</li><br>' +
         '<li>Al final se muestra la tabla de posiciones, donde el jugador con más puntos será el ganador.</li><br>' +
-    '</ol>',/*CREANDO LISTA ORDENADA*/
-        imageURL: 'log.png',
-        imageWidth: 400,
-        imageHeight: 200,
+    '</ol>',
+        imageUrl: '../recursos/img/logo.png',
+        imageWidth: 100,
+        imageHeight: 100,
         confirmButtonText: 'OK',
-        confirmButtonColor: '#ff0000',
+        confirmButtonColor: '#7B1FA2',
+        background: '#333333', 
         width: '800px',
+        customClass: {
+            title: 'custom-title',
+        }
     });
 }
 
